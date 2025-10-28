4c6a158e3f13460304a933e594afef92df9d545a
able03 <magicforyou18@gmail.com>
2025-10-29 00:02:39 +0800

    multiple added to app.jsx

--- client/gumira-test-ar/src/App.jsx
+++ client/gumira-test-ar/src/App.jsx
@@ -2,11 +2,12 @@ import './App.css'
 import Cube from './components/Cube'
 import Customization from './components/Customization'
 import ModelViewer from './components/ModelViewer'
+import MultipleFurnitureCustomization from './components/MultipleFurnitureCustomization'
 function App() {
 
   return (
     <>
-      <Customization/>
+      <MultipleFurnitureCustomization/>
       
     </>
   )
